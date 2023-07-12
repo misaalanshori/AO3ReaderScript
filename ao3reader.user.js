ebb7870793b9296a3fcf952780aa090291177704
M Isa Al Anshori <isabungsu@gmail.com>
2023-07-12 19:40:50 +0700

    Initial Commit

---
+++ ao3reader.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         AO3 Reader
+// @namespace    pw.misa
+// @version      0.1
+// @description  UI
+// @author       misa.pw
+// @match        https://archiveofourown.org
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=archiveofourown.org
+// @grant        none
+// @run-at       document-idle
+// @require      https://cdnjs.cloudflare.com/ajax/libs/localforage/1.10.0/localforage.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
