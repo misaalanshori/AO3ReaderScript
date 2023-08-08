8365efed3a582d6de1b5d53325c671182205b536
M Isa Al Anshori <isabungsu@gmail.com>
2023-08-08 07:49:15 +0700

    added a work list

--- src/app.jsx
+++ src/app.jsx
@@ -8,25 +8,50 @@ import refreshIcon from './assets/refresh.svg';
 
 import {GM} from '$';
 
+
+
+function WorkList({ works }) {
+  return (
+    <div className="a3r-workList">
+      {works.map(work => (
+        <div className="a3r-workItem">
+          <div className="a3r-workItem-header">
+            <h4 ><strong>{work.getLatest().title}</strong> by {work.getLatest().authors.map(e => e.name).join(" ,")}</h4>
+          </div>
+          <div className="a3r-workItem-tags"></div>
+          <div className="a3r-workItem-summary">
+            <h5><strong>Summary:</strong></h5>
+            <p>{work.getLatest().summary}</p>
+          </div>
+
+          
+        </div>
+      ))}
+    </div>
+  )
+}
+
+
 export function App({ closeApp }) {
-  const [count, setCount] = useState(0);
+  const [works, setWorks] = useState([]);
+
+  async function refresh() {
+    await Library.refresh();
+    setWorks(Library.works)
+  }
 
   useEffect(() => {
-    async function init() {
-      const count = await GM.getValue('count');
-      setCount(count||0);
-    }
-    init();
+    refresh();
   }, []);
 
   return (
     <div class="a3r-container">
       <div className="a3r-appbar">
         <div className="left">
-          <h5>AO3 Reader</h5>
+          <h3>AO3 Reader</h3>
         </div>
         <div className="right">
-          <button class="a3r-button" onClick={Library.refresh}>
+          <button class="a3r-button" onClick={refresh}>
             <img class="a3r-icons" src={refreshIcon} alt="refresh" />
           </button>
           <button class="a3r-button" onClick={closeApp}>
@@ -36,7 +61,7 @@ export function App({ closeApp }) {
       </div>
 
       <div className="a3r-contents">
-
+        <WorkList works={works}/>
       </div>
     </div>
   );
